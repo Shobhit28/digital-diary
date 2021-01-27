74809d6e22cde9890702e1ade68cbaaed7163103
AshnaAgarwal <64163352+AshnaAgarwal@users.noreply.github.com>
2021-01-27 19:57:35 +0530

    neww

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BranchMembersComponent } from './branch-members/branch-members.component';
+import { FilterPipe } from './filter.pipe';
+import { FormsModule }   from '@angular/forms';
 
 
 @NgModule({
@@ -14,11 +17,14 @@ import { BranchMembersComponent } from './branch-members/branch-members.componen
     AppComponent,
     DashboardComponent,
     PageNotFoundComponent,
-    BranchMembersComponent
+    BranchMembersComponent,
+    FilterPipe
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/branch-members/branch-members.component.ts
+++ src/app/branch-members/branch-members.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-branch-members',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BranchMembersComponent implements OnInit {
 
-  constructor() { }
+  @Input() formpersonal:FormGroup;
+  searchText:string='';
+  selectedMembers: string='';
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('',Validators.required),
+    mail: new FormControl('',Validators.required),
+    br: new FormControl(''),
+    mid: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
+  });
 
-  ngOnInit() {
+
+  characters:any[] = [
+    {name:'Ashi'},
+    {name:'Ashna'},
+    {name:'Astha'},
+    {name:'pooja'},
+    {name:'Tarika'},
+    {name:'Shreya'},
+    {name:'Apoorv'},
+    {name:'Poorva'},
+  ]
+  onClickEntry(name){
+    this.selectedMembers=name;
+}
+
+openform=false;
+ onClickOpenForm(){
+  this.openform=true;
+  return this.openform;
   }
 
+onSubmit() {
+  console.warn(this.formpersonal.value);
 }
+
+ngOnInit() {
+}
+}
+
+
+
+
+  
+
+
+
